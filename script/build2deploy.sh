d7f49ff751caeea2d031ac4d27b795818d7b37ba
Fellipe Souto <fllsouto@gmail.com>
2016-04-09 16:56:11 -0300

    Fixing structure, now I will deploy by myself.

--- script/build2deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname "${BASH_SOURCE}")
-pwd
-ls
-tar -czf package.tar.gz _site
-ls
-export SSHPASS=$REDELINUXPASS
-sshpass -e scp -v package.tar.gz fsouto@linux.ime.usp.br:/home/bcc/fsouto/tcc-test-foo
-sshpass -e ssh -v fsouto@linux.ime.usp.br sh /home/bcc/fsouto/tcc-test-foo/deploy.sh
--- script/jekyll_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONFIG_FILE="_prod_config"
-
-set -x
-cd $(dirname "${BASH_SOURCE}")
-jekyll clean
-jekyll build --config _prod_config.yml
