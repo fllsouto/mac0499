2fd499f2e28364b5551c9919570842ad803b87bd
Fellipe Souto <fllsouto@gmail.com>
2016-11-18 11:30:57 -0200

    Adding repo to automatically clone and deploy page on redelinux

---
+++ utils/clone_repo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+REDELINUX_USER_FOLDER='/home/bcc/fsouto/www/mac0499'
+
+git clone https://github.com/fllsouto/mac0499.git
+cd mac0499
+sh utils/deploy_page.sh $REDELINUX_USER_FOLDER
+cd ..
+rm -vrf mac0499/
